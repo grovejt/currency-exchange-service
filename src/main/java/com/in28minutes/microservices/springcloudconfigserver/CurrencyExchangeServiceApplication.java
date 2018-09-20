ec6ffe8614cfc54f7aba5088c676839471c426ec
grovejt <grovejt@gmail.com>
2018-09-20 10:55:05 -0500

    Initial Commit.

---
+++ src/main/java/com/in28minutes/microservices/springcloudconfigserver/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,12 @@
+package com.in28minutes.microservices.springcloudconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+}
